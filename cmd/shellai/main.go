4388af7fd3732ddd2c05cbb27271797d79bd8e43
SachinVenugopalan30 <sachin.venugopal795@gmail.com>
2026-04-30 13:39:27 -0700

    feat: add version argument to display version number

--- cmd/shellai/main.go
+++ cmd/shellai/main.go
@@ -13,13 +13,25 @@ import (
 // version is injected at build time via -ldflags "-X main.version=vX.Y.Z"
 var version = "dev"
 
+func printVersion() {
+	fmt.Printf("shellai %s (%s, %s/%s)\n", version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+}
+
 func main() {
+	// handle --version / -v before cobra parses (root requires 1 arg)
+	for _, a := range os.Args[1:] {
+		if a == "--version" || a == "-v" {
+			printVersion()
+			return
+		}
+	}
+
 	// version subcommand
 	cmd.Root.AddCommand(&cobra.Command{
 		Use:   "version",
 		Short: "Print version information",
 		Run: func(_ *cobra.Command, _ []string) {
-			fmt.Printf("shellai %s (%s, %s/%s)\n", version, runtime.Version(), runtime.GOOS, runtime.GOARCH)
+			printVersion()
 		},
 	})
 
